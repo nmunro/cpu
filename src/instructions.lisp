de6efd538c5c9aebc5ba576f4df9c202d221a71b
Neil Munro <nmunro@Freja.lan>
2020-09-14 00:15:25 +0100

    Added more instructions and a basic m68k hello world test program

--- src/instructions.lisp
+++ src/instructions.lisp
@@ -1,6 +1,9 @@
 (defpackage cpu.instructions
   (:use :cl)
   (:export #:move
+           #:lea
+           #:nop
+           #:trap
            #:add
            #:sub
            #:mul
@@ -17,6 +20,19 @@
   (multiple-value-bind (x y)
       (floor location)
     (setf (aref (cpu.memory:locations memory) y x) val)))
+
+(defun nop ())
+
+(defun lea (cpu src dest)
+  (setf (cpu.cpu:val (cpu.cpu:register dest cpu)) src))
+
+(defun trap (cpu memory trap-code)
+  (when (= #xf trap-code)
+    (let ((trap-task (cpu.cpu:val (cpu.cpu:register :d0 cpu))))
+      (cond
+        ((= 9  trap-task) (cl-user::quit))
+        ((= 13 trap-task) (format t "~A~%" (cpu.memory:address memory (cpu.cpu:val (cpu.cpu:register :d0 cpu)))))
+        ((= 14 trap-task) (format t "~A"   (cpu.memory:address memory (cpu.cpu:val (cpu.cpu:register :d0 cpu)))))))))
 
 (defun add (cpu destination source1 source2)
   (setf (cpu.cpu:val (cpu.cpu:register destination cpu)) (+ (cpu.cpu:val (cpu.cpu:register source1 cpu)) (cpu.cpu:val (cpu.cpu:register source2 cpu)))))
