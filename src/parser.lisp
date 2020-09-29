3bbd082ecaa88652438b62b1dd0a1989b9cb8ff2
NMunro <neilmunro@gmail.com>
2020-09-29 09:49:58 +0100

    #14 Started adding functions for beginning to parse code
    #14 Started adding functions for beginning to parse code

--- src/parser.lisp
+++ src/parser.lisp
@@ -4,9 +4,30 @@
 
 (defun read-file (path)
   (with-open-file (stream path :direction :input :if-does-not-exist nil)
-    (loop for line = (read-line stream nil)
-          while line
-          collect line)))
+    (loop for line = (read-line stream nil) while line collect line)))
 
-(dolist (line (read-file #p"~/quicklisp/local-projects/cpu/examples/hello-world.asm"))
-  (format t "~A~%" line))
+(defun map-to-memory (lines)
+  (dolist (line lines)
+    (format t "~{~A~^, ~}~%" line)))
+
+(defun translate (line)
+  `(,(getf line :label) ,(getf line :function) ,@(uiop:split-string (first (getf line :args)) :separator ",")))
+
+(defun parse-line (line)
+  (let ((data (remove-if #'(lambda (x) (string= x "")) (uiop:split-string line :separator " ") :start 1)))
+    (translate `(:label    ,(first data)
+                :function ,(cadr data)
+                :args     ,(cdr (remove-if (lambda (word) (string= "" word)) (rest data)))))))
+
+(defun remove-junk (line)
+  (cond
+    ((string= "" (string-trim " " line))
+     nil)
+
+    ((string= "*-" (subseq line 0 2))
+     nil)
+
+    (t line)))
+
+(let ((code (read-file #p"~/quicklisp/local-projects/cpu/examples/hello-world.asm")))
+    (map-to-memory (mapcar #'parse-line (remove-if (complement #'remove-junk) code))))
